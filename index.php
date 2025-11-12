129544bea42b05eb13ed39a56bb926f4cf8eed8d
Nguyễn Minh Phú <minhphu025405@gmail.com>
2025-11-12 15:52:22 +0700

    project1

---
+++ index.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>auto99</title>
+  <link rel="stylesheet" type="text/css" href="./admincp/asset/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css " href="./admincp/asset/bootstrap/css/style_phu.css">
+  
+</head>
+<script src="./admincp/asset/bootstrap/js/bootstrap.bundle.min.js"></script>
+<body>
+    <div class="wrapper">
+      <!-- header -->
+      <div class="header">     
+        <img src="./admincp/images/Audi9.jpg" width="100%" height="120px"  alt="Ảnh ví dụ">
+
+      </div>
+      <!-- menu -->
+      <div class="menu"> 
+          <nav class="navbar navbar-expand-lg bg-body-tertiary">
+              <div class="container-fluid">
+                <a class="navbar-brand" href="http://localhost:8888/hoc_php/">AUTO99</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse " id="navbarNav">
+                  <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                      <a class="nav-link active" aria-current="page" href="http://localhost:8888/hoc_php/">TRANG CHU</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">GIOI THIEU</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">TIN TUC XE</a>
+                    </li> 
+                    <li class="nav-item">
+                      <a class="nav-link" href="">LIEN HE</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+          </nav>
+      </div>
+      <!-- main -->
+      <div class="main"> 
+        <div class="sidebar">
+          <ul class="menu-sidebar" >
+                    <li class="nav-item">
+                      <a class="nav-link active" aria-current="page" href="http://localhost:8888/hoc_php/">
+                        <img src="https://auto88.vn/uploads/salon/images/logo/peugeot.png" alt="">
+                        <p>Peugeot</p>
+                      </a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">
+                        <img src="	https://auto88.vn/uploads/salon/images/logo/mercedes_benz.png" alt="">
+                        <p>Mescedes Benz</p>
+                      </a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">
+                        <img src="	https://auto88.vn/uploads/salon/images/logo/honda.png" alt="">
+                        <p>Honda</p>
+                      </a>
+                    </li> 
+                    <li class="nav-item">
+                      <a class="nav-link" href="">
+                        <img src="https://auto88.vn/uploads/salon/images/logo/toyota.png" alt="">
+                        <p>Toyota</p>
+                      </a>
+                    </li>
+            </ul>
+        </div>
+
+        <div class="maincontent">
+          <div class="card-item">
+            <ul class="list-item">
+                <li class="body-item">
+                  <a  href="#">
+                    <img src="https://www.peugeotvietnam.vn/content/dam/peugeot/vietnam/trang-san-pham-2008-mca/thumbnail/new-peugeot-2008-THUMBNAIL-600x600.png" alt="">   
+                         New PEUGEOT 2008            
+                  </a>
+                  <span>900.000.000VND</span>     
+                </li>
+
+                <li class="body-item">
+                  <a  href="#">
+                    <img src="https://www.peugeotvietnam.vn/content/dam/peugeot/vietnam/thumbnail-xe/trang-baseball-card/THUMBNAIL-600x600-3008-new-logo.png" alt="">   
+                         New PEUGEOT 3008            
+                  </a>
+                  <span>900.000.000VND</span>     
+                </li>
+
+                 <li class="body-item">
+                  <a  href="#">
+                    <img src="https://www.peugeotvietnam.vn/content/dam/peugeot/vietnam/thumbnail-xe/trang-baseball-card/newpeugeot-408-THUMBNAIL-600x600.png" alt="">   
+                         PEUGEOT 408           
+                  </a>
+                  <span>900.000.000VND</span>     
+                </li>
+
+                 <li class="body-item">
+                  <a  href="#">
+                    <img src="https://www.peugeotvietnam.vn/content/dam/peugeot/vietnam/thumbnail-xe/trang-baseball-card/THUMBNAIL-600x600-5008-new-logo.png" alt="">   
+                         New PEUGEOT 5008            
+                  </a>
+                  <span>900.000.000VND</span>     
+                </li>
+
+                 <li class="body-item">
+                  <a  href="#">
+                    <img src="https://www.peugeotvietnam.vn/content/dam/peugeot/vietnam/trang-san-pham-2008-mca/thumbnail/new-peugeot-2008-THUMBNAIL-600x600.png" alt="">   
+                         New PEUGEOT 2008            
+                  </a>
+                  <span>900.000.000VND</span>     
+                </li>
+
+            </ul>
+
+          </div>
+        </div>
+      </div>
+
+    </div>
+  
+</body>
+<footer class="bg-dark text-white text-center py-3 mt-auto">
+  footer
+</footer>
+</html>
